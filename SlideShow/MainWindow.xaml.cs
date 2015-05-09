80d63fc4e7bf0fb75823728903f9d91bf58a42e1
Dmitry Efimov <d.efimov@simcase.ru>
2015-05-09 18:57:16 +0300

    Save and open slideshow in xml files.

--- SlideShow/MainWindow.xaml.cs
+++ SlideShow/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace SlideShow
 {
@@ -22,9 +23,11 @@ namespace SlideShow
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> pictures;
         public MainWindow()
         {
             InitializeComponent();
+            pictures = new List<string>();
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
@@ -47,6 +50,8 @@ namespace SlideShow
                     smallImage.Height = 80;
                     ListBox.Items.Add(smallImage);
 
+                    pictures.Add(loadImage.UriSource.AbsolutePath);
+
                     return;
                 }
                 catch (Exception ex)
@@ -67,6 +72,87 @@ namespace SlideShow
                 loadImage.EndInit();
 
                 bigImage.Source = loadImage;
+            }
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    FileInfo fileInfo = new FileInfo(saveFileDialog.FileName);
+
+                    MySlideShow slideShow = new MySlideShow();
+                    slideShow.Head = new Head();
+                    slideShow.Head.Title = "SlideShow1";
+
+                    slideShow.Body = new List<Slide>();
+
+                    int count = ((System.Windows.Controls.ItemsControl)(ListBox)).Items.Count;
+
+                    foreach(var p in pictures) 
+                    {
+                        slideShow.Body.Add(new Slide() { FileInfo = p, Time = 1000, Type = "picture" });
+                    }
+
+                    XmlSerializer formatter = new XmlSerializer(typeof(MySlideShow));
+                    FileStream fs = new FileStream(fileInfo.FullName, FileMode.OpenOrCreate);
+                    formatter.Serialize(fs, slideShow);
+
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            pictures = new List<string>();
+            ListBox.Items.Clear();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+
+                    XmlSerializer formatter = new XmlSerializer(typeof(MySlideShow));
+                    MySlideShow slideShow;
+                    using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.OpenOrCreate))
+                    {
+                        slideShow = (MySlideShow)formatter.Deserialize(fs);
+                    }
+
+                    foreach (var slide in slideShow.Body) 
+                    {
+                        FileInfo fileInfo2 = new FileInfo(slide.FileInfo.Replace("%20", " "));
+
+                        BitmapImage loadImage = new BitmapImage();
+                        loadImage.BeginInit();
+                        loadImage.UriSource = new Uri(fileInfo2.FullName);
+                        loadImage.EndInit();
+
+                        Image smallImage = new Image();
+                        smallImage.Source = loadImage;
+                        smallImage.Height = 80;
+                        ListBox.Items.Add(smallImage);
+
+                        pictures.Add(loadImage.UriSource.AbsolutePath);
+                    }
+
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
             }
         }
     }
